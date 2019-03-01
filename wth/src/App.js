a1af3ad370ffd4abeb604314f5de261c92426173
Ilias Swinnen <swinnen.ilias@gmail.com>
2019-03-01 19:18:32 +0100

    Start of Routes

--- wth/src/App.js
+++ wth/src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/Home';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <HomePage></HomePage>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<BrowserRouter>
+					<Switch>
+						<Route to="/" exact component={HomePage} />
+            <Route to="/login" exact component={HomePage}></Route>
+            <Route to="/register" exact component={HomePage}/>
+					</Switch>
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
 
 export default App;
