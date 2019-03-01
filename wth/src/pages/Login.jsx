0b4415b4c708ec040809e9a832beade2f97fe95c
Ilias Swinnen <swinnen.ilias@gmail.com>
2019-03-01 22:17:41 +0100

    Firebase auth: login and register

--- wth/src/pages/Login.jsx
+++ wth/src/pages/Login.jsx
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
+import fire from '../Fire';
 
 class Login extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			username: '',
+			email: '',
 			password: '',
 		};
+
+		this.login = this.login.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleChange(e) {
-		this.setState({ [e.target.name]: e.target.value });
+		let target = e.target;
+		let value = target.value;
+		let name = target.name;
+
+		this.setState({
+			[name]: value,
+		});
+	}
+
+	login(e) {
+		e.preventDefault();
+		fire.auth()
+			.signInWithEmailAndPassword(this.state.email, this.state.password)
+			.then(u => {})
+			.catch(error => {
+				console.log(error);
+			});
 	}
 
 	render() {
 		return (
 			<div>
-				{console.log(this.state.username)}
+				{console.log(this.state.email)}
 				{console.log(this.state.password)}
-				<input type="text" value={this.state.username} />
-				<input type="password" value={this.state.password} />
+				<input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
+				<input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+				<button onClick={this.login}>Login</button>
 			</div>
 		);
 	}
