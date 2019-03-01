1495d4d1b0c83a6bc579f11957ea2f594db53816
Ilias Swinnen <swinnen.ilias@gmail.com>
2019-03-01 21:06:46 +0100

    Start adding firebase auth

--- wth/src/App.js
+++ wth/src/App.js
@@ -4,12 +4,27 @@ import './App.css';
 import HomePage from './pages/Home';
 import CourseChooser from './pages/CourseChooser';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import fire from './Fire.js';
+import Login from './pages/Login';
 
 class App extends Component {
+	authListener() {
+		fire.auth().onAuthStateChanged(user => {
+			console.log(user);
+			if (user) {
+				this.setState({ user });
+				localStorage.setItem('user', user.uid);
+			} else {
+				this.setState({ user: null });
+				localStorage.removeItem('user');
+			}
+		});
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<CourseChooser/>
+				<Login />
 			</div>
 		);
 	}
