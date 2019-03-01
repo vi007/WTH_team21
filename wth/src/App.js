bddba75e9e96527fee3e0e69311dc0ad5da0860c
Vanluyten Vicky <vvanluyten@yahoo.com>
2019-03-01 20:49:46 +0100

    courseChooser added

--- wth/src/App.js
+++ wth/src/App.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/Home';
+import CourseChooser from './pages/CourseChooser';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<BrowserRouter>
-					<Switch>
-						<Route to="/" exact component={HomePage} />
-            <Route to="/login" exact component={HomePage}></Route>
-            <Route to="/register" exact component={HomePage}/>
-					</Switch>
-				</BrowserRouter>
+				<CourseChooser/>
 			</div>
 		);
 	}
