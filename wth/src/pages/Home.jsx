d9609c491c954d5bc186ea6b979e39a60904296b
Ilias Swinnen <swinnen.ilias@gmail.com>
2019-03-01 18:43:36 +0100

    First files

---
+++ wth/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Homepage</div>
+         );
+    }
+}
+ 
+export default HomePage;
