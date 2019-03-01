3f4cdd6995f8c6b1b2c160462d3d383550c959a3
Maxwell Szymanski <maxwell.szymanski@student.kuleuven.be>
2019-03-01 18:56:00 +0100

    First Pages

--- wth/src/pages/Home.jsx
+++ wth/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class HomePage extends Component {
-    state = {  }
+    state = {  };
     render() { 
         return ( 
             <div>Homepage</div>
---
+++ wth/src/pages/Settings.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+    state = {  };
+    render() {
+        return (
+            <div>Settings</div>
+        );
+    }
+}
+
+export default Settings;
