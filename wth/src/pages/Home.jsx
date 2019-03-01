d011b2052831039b36707b1c3a8b0628eb4a4419
Maxwell Szymanski <maxwell.szymanski@student.kuleuven.be>
2019-03-01 22:00:42 +0100

    Test

--- wth/src/pages/Home.jsx
+++ wth/src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class HomePage extends Component {
-    state = {  };
+    constructor(props) {
+        super(props);
+        this.state = {
+            subjects: []
+        };
+
+    }
+
+    renderSubjects() {
+        let html = [];
+        for (let i = 1; i <= 4; i++) {
+            html.push(<div key={i} className="attributeValueActive"> </div>)
+        }
+    }
+
     render() { 
         return ( 
             <div>
-                <button> Hello </button>
+
             </div>
          );
     }
