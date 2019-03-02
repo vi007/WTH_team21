4a7ac93eaf513ff763c1170f8dd4d254518a4c60
Vanluyten Vicky <vvanluyten@yahoo.com>
2019-03-02 10:22:10 +0100

    settings page

--- wth/src/pages/Settings.jsx
+++ wth/src/pages/Settings.jsx
@@ -17,11 +17,50 @@ class Settings extends Component {
 
     render() {
         return (
-            <div><h1> Settings </h1>
-                <div className="field">
+            <div className='settingsPage'>
+                
+                <h1 className='headSettings'> Settings </h1>
+                
+                
+                <div className='switchField' style={{color: 'grey'}}>
                     <input id="switchRoundedDefault" type="checkbox" name="switchRoundedDefault" className="switch is-rounded" checked={this.state.DarkMode} onChange={this.changedvalue}/>
                     <label for="switchRoundedDefault">Activate Dark mode</label>
-                </div>
+                </div> <br/><br/>              
+          
+                
+                <div className='switchLink' style={{color: 'grey'}}>
+
+                    <label>Automatic Dark Mode</label>
+                </div> <br/><br/> 
+                
+                <div className='line' style={{color: 'grey'}}>
+                </div> <br/><br/> 
+                
+                <div className='switchField' style={{color: 'grey'}}>
+                    <input id="switchRoundedDefault" type="checkbox" name="switchRoundedDefault" className="switch is-rounded" checked={this.state.DarkMode} onChange={this.changedvalue}/>
+                    <label for="switchRoundedDefault">Notifications</label>
+                </div> <br/><br/> 
+                
+                <div className='switchField' style={{color: 'grey'}}>
+                    <label for="switchRoundedDefault">Notification Settings</label>
+                </div> <br/><br/>
+                
+                <div className='line' style={{color: 'grey'}}>
+                </div> <br/><br/> 
+                
+                <div className='switchField' style={{color: 'grey'}}>
+                    <label for="switchRoundedDefault">Personal Information</label>
+                </div> <br/><br/>
+                
+                <div className='switchField' style={{color: 'grey'}}>
+                    <label for="switchRoundedDefault">Color Setting</label>
+                </div> <br/><br/>
+                
+                <div className='switchField' style={{color: 'grey'}}>
+                    <label for="switchRoundedDefault">Reset</label>
+                </div> <br/><br/>
+                
+                
             </div>
         );
     }
