d12c2c71e5f494883431e8d3ae5e589be6bfe59f
Maxwell Szymanski <maxwell.szymanski@student.kuleuven.be>
2019-03-02 10:25:18 +0100

    Hover

--- wth/src/pages/Settings.jsx
+++ wth/src/pages/Settings.jsx
@@ -30,7 +30,7 @@ class Settings extends Component {
                 
                 <div className='switchLink' style={{color: 'grey'}}>
 
-                    <label>Automatic Dark Mode</label>
+                    <label className="hoverColor">Automatic Dark Mode</label>
                 </div> <br/><br/> 
                 
                 <div className='line' style={{color: 'grey'}}>
