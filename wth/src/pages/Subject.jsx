59a424d3b1778a210bfa919bdab66acd6387aa56
Ilias Swinnen <swinnen.ilias@gmail.com>
2019-03-02 12:00:53 +0100

    Linking things

--- wth/src/pages/Subject.jsx
+++ wth/src/pages/Subject.jsx
@@ -1,77 +1,83 @@
 import React, { Component } from 'react';
 import CourseTab from './CourseTab';
 //import BulmaCheckradio from "bulma-checkradio";
-import bulmaSwitch from "bulma-switch"; // Laten staan, wordt wel gebruikt zelf al zegt hij van niet.
+import bulmaSwitch from 'bulma-switch'; // Laten staan, wordt wel gebruikt zelf al zegt hij van niet.
 
 class Subject extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            subjectName: 'Course Name',
-            selectedTab: 0, // 0 = Lectures, 1= Exercises, 2= Studying
-            checkLectures: false,
-            checkExercises: true,
-            checkStudying: false,
-            chapters: ['Hoofdstuk 1', 'Hoofdstuk 2']
-        };
+	constructor(props) {
+		super(props);
+		this.state = {
+			subjectName: 'Course Name',
+			selectedTab: 0, // 0 = Lectures, 1= Exercises, 2= Studying
+			checkLectures: false,
+			checkExercises: true,
+			checkStudying: false,
+			chapters: ['Hoofdstuk 1', 'Hoofdstuk 2'],
+		};
 
-        this.changedvalueLecture = this.changedvalueLecture.bind(this);
-        this.changedvalueExercises = this.changedvalueExercises.bind(this);
-        this.changedvalueStudying = this.changedvalueStudying.bind(this);
-    }
+		this.changedvalueLecture = this.changedvalueLecture.bind(this);
+		this.changedvalueExercises = this.changedvalueExercises.bind(this);
+		this.changedvalueStudying = this.changedvalueStudying.bind(this);
+	}
 
+	changedvalueLecture() {
+		this.setState({ selectedTab: 0 });
+		this.setState({ checkLectures: true });
+		this.setState({ checkExercises: false });
+		this.setState({ checkStudying: false });
+		console.log('lecture');
+	}
+	changedvalueExercises() {
+		this.setState({ selectedTab: 1 });
+		this.setState({ checkLectures: false });
+		this.setState({ checkExercises: true });
+		this.setState({ checkStudying: false });
+	}
+	changedvalueStudying() {
+		this.setState({ selectedTab: 2 });
+		this.setState({ checkLectures: false });
+		this.setState({ checkExercises: false });
+		this.setState({ checkStudying: true });
+	}
 
-    changedvalueLecture(){
-        this.setState({selectedTab: 0});
-        this.setState({checkLectures: true});
-        this.setState({checkExercises: false});
-        this.setState({checkStudying: false});
-        console.log("lecture");
-    }
-    changedvalueExercises(){
-        this.setState({selectedTab: 1});
-        this.setState({checkLectures: false});
-        this.setState({checkExercises: true});
-        this.setState({checkStudying: false});
-    }
-    changedvalueStudying(){
-        this.setState({selectedTab: 2});
-        this.setState({checkLectures: false});
-        this.setState({checkExercises: false});
-        this.setState({checkStudying: true});
-    }
-
-
-
-    render() {
-        return (
-            <div className="">
-                <h1> {this.state.subjectName} </h1>
-                <div>
-
-
-                        /*<input className="is-checkradio is-danger" id="exampleCheckboxDanger" type="checkbox" name="exampleCheckboxDanger" checked={this.state.checkLectures} onChange={this.changedvalueLecture}/>
-                        <label htmlFor="exampleCheckboxDanger">Lectures</label>
-
-                        <input className="is-checkradio is-danger" id="exampleCheckboxDanger" type="checkbox" name="exampleCheckboxDanger" checked={this.state.checkExercises} onChange={this.changedvalueExercises}/>
-                        <label htmlFor="exampleCheckboxDanger">Exercises</label>
-
-                        <input className="is-checkradio is-danger" id="exampleCheckboxDanger" type="checkbox" name="exampleCheckboxDanger"checked={this.state.checkStudying} onChange={this.changedvalueStudying}/>
-                        <label htmlFor="exampleCheckboxDanger">Studying</label>*/
-
-
-                        
-
-
-                    
-                    <CourseTab>
-
-                    </CourseTab>
-
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="">
+				<h1> {this.props.course.name} </h1>
+				<div>
+					/*
+					<input
+						className="is-checkradio is-danger"
+						id="exampleCheckboxDanger"
+						type="checkbox"
+						name="exampleCheckboxDanger"
+						checked={this.state.checkLectures}
+						onChange={this.changedvalueLecture}
+					/>
+					<label htmlFor="exampleCheckboxDanger">Lectures</label>
+					<input
+						className="is-checkradio is-danger"
+						id="exampleCheckboxDanger"
+						type="checkbox"
+						name="exampleCheckboxDanger"
+						checked={this.state.checkExercises}
+						onChange={this.changedvalueExercises}
+					/>
+					<label htmlFor="exampleCheckboxDanger">Exercises</label>
+					<input
+						className="is-checkradio is-danger"
+						id="exampleCheckboxDanger"
+						type="checkbox"
+						name="exampleCheckboxDanger"
+						checked={this.state.checkStudying}
+						onChange={this.changedvalueStudying}
+					/>
+					<label htmlFor="exampleCheckboxDanger">Studying</label>*/
+					<CourseTab />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Subject;
--- wth/src/pages/SubjectList.jsx
+++ wth/src/pages/SubjectList.jsx
@@ -8,28 +8,28 @@ class SubjectList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			subjects: ['Algebra', 'Fysica', 'Wiskunde'],
 			descriptor: ['Due in 3 days', 'Due to tomorrow', 'Due in 2 days'],
-			data: [],
+            data: [],
+            subject: null
 		};
 
 		this.getCourses = this.getCourses.bind(this);
-    }
-    
-    componentDidMount(){
-        this.getCourses();
-    }
+	}
+
+	componentDidMount() {
+		this.getCourses();
+	}
 
 	generateSubjects() {
 		let html = [];
-		for (let i = 0; i < this.state.subjects.length; i++) {
+		for (let i = 0; i < this.state.data.length; i++) {
 			let fade = 'subjectItem  fadeIn' + i;
 			html.push(
-				<div className={fade}>
+                    <div className={fade} key={i} onClick={() => this.setState({subject: this.state.data[i]})}>
 					<div className="subjectPhoto"> </div>
-					<div key={i} className="subjectLabel ">
+					<div className="subjectLabel ">
 						{' '}
-						{this.state.subjects[i]}
+						{this.state.data[i].name}
 					</div>
 					<br />
 					<div className="subjectDescriptor"> {this.state.descriptor[i]}</div>
@@ -58,30 +58,27 @@ class SubjectList extends Component {
 	addCourse() {
 		firebase
 			.database()
-			.ref('courses/' + 'LA')
+			.ref('courses/' + 'RLZ')
 			.set({
-				name: 'Lineaire Algebra',
-				chapters: ['Hoofdstuk 1: Inleiding', 'Hoodstuk 2: Vectoren'],
+				name: 'Religie',
+				chapters: ['Hoofdstuk 1: Religie', 'Hoodstuk 2: Antropoceen'],
 			});
 	}
+
 
 	render() {
 		return (
 			<div>
-				SubjectList
-				<Subject />
-				
+				<h1 className="fadeIn0">Subjects </h1>
+                {this.state.subject == null && this.generateSubjects()}
+
+                {this.state.subject !== null && <Subject course={this.state.subject}></Subject>}
+
+                
+                <button onClick={this.addCourse}></button>
 			</div>
 		);
 	}
-
-	// render() {
-	// return (
-	// <div>
-	// <h1 className="fadeIn0">Subjects </h1>
-	// {this.generateSubjects()}
-	// </div>
-	// );
 }
 
 export default SubjectList;
