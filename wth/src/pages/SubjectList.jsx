3f536fe665348ac39851292041f3e7561e8fa06c
Maxwell Szymanski <maxwell.szymanski@student.kuleuven.be>
2019-03-02 03:59:53 +0100

    Little update

--- wth/src/pages/SubjectList.jsx
+++ wth/src/pages/SubjectList.jsx
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import Subject from './Subject';
 
 class SubjectList extends Component {
-    state = {  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            subjects: ["Algebra", "Fysica"]
+        }
+    }
+
+    generateSubjects(number) {
+        const active = Math.round(number / 100);
+        let html = [];
+        for (let i = 0; i < this.state.subjects.length; i++) {
+            html.push(<div key={i} className=""> {this.state.subjects[i]}</div>)
+        }
+        if (active !== 4) {
+            for (let j = 1; j <= 4-active; j++) {
+                html.push(<div key={5+j} className="attributeValue"> </div>)
+            }
+        }
+        return html
+    }
+
     render() { 
         return ( 
             <div>
-                SubjectList
-                <Subject>
-
-                </Subject>
+                <h1>Subjects </h1>
+                {this.generateSubjects()}
             </div>
          );
     }
